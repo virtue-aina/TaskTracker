3b4e2fa02bce6a0358910d657ce6de998270c206
Virtue Aina <ayokanmi.aina@gmail.com>
2025-06-06 23:28:23 +0100

    added entities, dockerised, and file structure

---
+++ src/main/java/dev/virtue/tasktracker/domain/entities/Task.java
@@ -0,0 +1,24 @@
+package dev.virtue.tasktracker;
+
+//import java.time.LocalDateTime;
+//import java.util.UUID;
+//
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Getter
+//@Setter
+//@ToString
+//@RequiredArgsConstructor
+//@Entity
+//public class Task {
+//    @Id
+//   private UUID id;
+//   private String title;
+//   private String description;
+//   private LocalDateTime deadline;
+//   private TaskPriority priority;
+//   private LocalDateTime created;
+//   private LocalDateTime updated;
+//   private TaskList taskList;
+//
+//}
