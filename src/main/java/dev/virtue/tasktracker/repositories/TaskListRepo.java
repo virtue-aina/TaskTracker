ab98c957c3e3ed4d1045d8e856fa219f113c2f03
Virtue Aina <ayokanmi.aina@gmail.com>
2025-06-12 08:27:56 +0100

    adjustments made to repo.

--- src/main/java/dev/virtue/tasktracker/repositories/TaskListRepo.java
+++ src/main/java/dev/virtue/tasktracker/repositories/TaskListRepo.java
@@ -1,17 +1,12 @@
 package dev.virtue.tasktracker.repositories;
 
-import dev.virtue.tasktracker.domain.entities.Task;
 import dev.virtue.tasktracker.domain.entities.TaskList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TaskListRepo extends JpaRepository<TaskList, UUID> {
-    List<Task> findTaskListById(UUID taskListId);
 
-    Optional<Task> findByTaskListIdandId(UUID taskListId, UUID taskId);
 }
--- src/main/java/dev/virtue/tasktracker/repositories/TaskRepo.java
+++ src/main/java/dev/virtue/tasktracker/repositories/TaskRepo.java
@@ -4,8 +4,14 @@ import dev.virtue.tasktracker.domain.entities.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TaskRepo extends JpaRepository<Task, UUID> {
+
+    List<Task> findTaskListById(UUID taskListId);
+
+    Optional<Task> findByTaskListIdandId(UUID taskListId, UUID taskId);
 }
