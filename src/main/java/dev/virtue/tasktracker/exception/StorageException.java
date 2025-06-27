8c9c403ffb6f18bcd31f83a15aefd405ee16bd42
Virtue Aina <ayokanmi.aina@gmail.com>
2025-06-27 21:53:37 +0100

    added storage service strategy

---
+++ src/main/java/dev/virtue/tasktracker/exception/StorageException.java
@@ -0,0 +1,15 @@
+package dev.virtue.tasktracker.exception;
+
+public class StorageException extends BaseException{
+    public StorageException() {
+        super();
+    }
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
