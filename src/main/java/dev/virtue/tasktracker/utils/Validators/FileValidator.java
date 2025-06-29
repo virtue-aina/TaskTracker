be3ea78542419a98956243be5b36958493ac8b73
Virtue Aina <ayokanmi.aina@gmail.com>
2025-06-29 01:21:12 +0100

    refactoring and modularising.

--- src/main/java/dev/virtue/tasktracker/utils/Validators/FileValidator.java
+++ src/main/java/dev/virtue/tasktracker/utils/Validators/FileValidator.java
@@ -1,8 +1,26 @@
 package dev.virtue.tasktracker.utils.Validators;
 
 import dev.virtue.tasktracker.exception.StorageException;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.List;
+
+@Component
 public class FileValidator {
+    /**
+     * List of allowed media types for file uploads.
+     * This list can be extended to include more types as needed.
+     */
+    private static final List<MediaType> ALL_ALLOWED_TYPES = Arrays.asList(
+            MediaType.IMAGE_JPEG, MediaType.IMAGE_PNG, MediaType.IMAGE_GIF,
+            MediaType.APPLICATION_PDF, MediaType.valueOf("application/msword"),
+            MediaType.valueOf("application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+    );
+
+
+
 
     public static String extensionChecker(String fileName) {
 
